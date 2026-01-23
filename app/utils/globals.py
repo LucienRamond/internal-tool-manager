768bc3932bcf449735ce54e2a3b38617e9f2ac2d
Lucien Ramond <lucien_ramond@hotmail.com>
2026-01-23 13:48:59 +0100

    Create a globals file for reusable var and enum

---
+++ app/utils/globals.py
@@ -0,0 +1,3 @@
+OWNER_DEPARTMENT = ["Engineering","Sales","Marketing","HR","Finance","Operations","Design"]
+TWO_DECIMAL_REGEX = "^[0-9]+([.,]\d{0,2})?$"
+VERIFY_DOMAIN_REGEX = "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
